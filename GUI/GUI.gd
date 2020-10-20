50085d6eac7f0f14718c2768452d32fb98b8bdd7
Daniel Lee <daniellee0217@yahoo.com>
2020-10-20 16:34:18 -0400

    Added main pawn indicator

--- GUI/GUI.gd
+++ GUI/GUI.gd
@@ -4,11 +4,12 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 export var minimap_gui_nodepath : NodePath
+var minimap_gui_node
 var minimap_node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var minimap_gui_node = get_node(minimap_gui_nodepath)
+	minimap_gui_node = get_node(minimap_gui_nodepath)
 	minimap_node = get_node("/root/environment/TestMap").minimapNode
 	minimap_node.get_parent().remove_child(minimap_node)
 	minimap_gui_node.add_child(minimap_node)
@@ -16,5 +17,8 @@ func _ready():
 	minimap_node.z_index = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var player_list = get_parent().get_parent().get_parent().pawnList
+	for pawn in player_list:
+		if pawn.name == "MainPawn":
+			minimap_gui_node.get_node("MainPawnIndicator").position = (pawn.position / 25)
