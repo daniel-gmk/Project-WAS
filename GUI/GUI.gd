91c475848976545bda3340eca8e152ac2db85712
Daniel Lee <daniellee0217@yahoo.com>
2020-10-22 00:40:22 -0400

    minion falling out of map doesnt remove the minimap icon
    enemy shows up when they fall out of the map
    
    mainpawn didnt turn invisible when falling out of map
    
    minions were not detected by visionmanager

--- GUI/GUI.gd
+++ GUI/GUI.gd
@@ -10,10 +10,12 @@ var minimapSize
 var minimapCamera
 var player_node
 var local = false
+var indicatorScript
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player_node = get_parent().get_parent().get_parent()
+	indicatorScript = preload("res://GUI/Indicator.gd")
 	if !get_tree().is_network_server() and int(player_node.get_parent().name) == get_tree().get_network_unique_id():
 		local = true
 		var testMap = get_node("/root/environment/TestMap")
@@ -31,28 +33,35 @@ func _process(delta):
 	if local and !player_node.get_node("TeleportManager").teleporting:
 		var player_list = player_node.pawnList
 		var mainPawnSize = player_node.get_node("MainPawn/BodyCollision").shape.height
+
+		for minion_indicator in get_tree().get_nodes_in_group("MinionIndicators"):
+			if player_list.find(minion_indicator.linkedPawn) == -1:
+				minion_indicator.queue_free()
 		for pawn in player_list:
 			if pawn.name == "MainPawn":
 				minimap_gui_node.get_node("MainPawnIndicator").position = (pawn.position / minimapSize)
-				minimap_gui_node.get_node("MainPawnIndicator").visible = pawn.visible
+				minimap_gui_node.get_node("MainPawnIndicator").visible = pawn.get_node("StateManager/Sprite").visible
 			else:
 				if !minimap_gui_node.has_node(pawn.name + "Indicator"):
 					var minion_indicator = Sprite.new()
 					minion_indicator.name = pawn.name + "Indicator"
 					minion_indicator.texture = load("res://assets/minimap-minion-indicator.png")
 					minion_indicator.z_index = 3
+					minion_indicator.set_script(indicatorScript)
 					var scaleVal = (pawn.get_node("BodyCollision").shape.height/mainPawnSize) / 4
+					minion_indicator.linkedPawn = pawn
 					minion_indicator.scale = Vector2(scaleVal, scaleVal)
 					minimap_gui_node.add_child(minion_indicator)
+					minion_indicator.add_to_group("MinionIndicators")
 				minimap_gui_node.get_node(pawn.name + "Indicator").position = (pawn.position / minimapSize)
-				minimap_gui_node.get_node(pawn.name + "Indicator").visible = pawn.visible
+				minimap_gui_node.get_node(pawn.name + "Indicator").visible = pawn.get_node("StateManager/Sprite").visible
 
-		var global_camera = (get_node("/root/").size * get_parent().get_parent().zoom)
+		var global_camera = (get_viewport().get_visible_rect().size * get_parent().get_parent().zoom)
 		minimapCamera.scale = Vector2((global_camera.x / minimapSize) / minimapCamera.texture.get_width(), (global_camera.y / minimapSize) / minimapCamera.texture.get_height())
 		minimapCamera.position = get_parent().get_parent().position / minimapSize
 		var currentPawn = player_node.currentActivePawn
 		minimap_gui_node.get_node("SelectedPawnIndicator").position = Vector2(currentPawn.position.x / minimapSize, (currentPawn.position.y - (currentPawn.get_node("BodyCollision").shape.height) * 13) / minimapSize)
-		minimap_gui_node.get_node("SelectedPawnIndicator").visible = currentPawn.visible
+		minimap_gui_node.get_node("SelectedPawnIndicator").visible = currentPawn.get_node("StateManager/Sprite").visible
 
 		var enemy_list = get_tree().get_nodes_in_group("OnScreenEntities")
 		if enemy_list.size() > 0:
@@ -77,4 +86,4 @@ func _process(delta):
 					enemy_indicator.scale = Vector2(scaleVal, scaleVal)
 					minimap_gui_node.add_child(enemy_indicator)
 				minimap_gui_node.get_node(enemy_indicator_name).position = (enemy.position / minimapSize)
-				minimap_gui_node.get_node(enemy_indicator_name).visible = enemy.visible
+				minimap_gui_node.get_node(enemy_indicator_name).visible = enemy.get_node("StateManager/Sprite").visible
